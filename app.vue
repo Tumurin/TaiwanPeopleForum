0422ae7d9e6a3fdc6cd556c976a14957d3c026bd
Tumurin <kuanminzhao@gmail.com>
2024-02-09 21:30:24 +0800

    側邊欄 圖標 LOGO Hover淡入效果

--- app.vue
+++ app.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
-    <h1 class="text-red-500">hello Taiwan</h1>
+  <div :class="{ dark: darkMode }">
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+          <!-- 左 -->
+          <div class="md:block xs:col-span-1 xl:col-span-2">
+            <div class="sticky top-0">
+              <SidebarLeft />
+            </div>
+          </div>
+          <!-- 中 -->
+
+          <!-- 右 -->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup>
+const darkMode = ref(false);
+</script>
