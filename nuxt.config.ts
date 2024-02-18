9b7e4dd8a182bd04dfa16166079f52e50dc1c24b
Tumurin <kuanminzhao@gmail.com>
2024-02-18 20:01:16 +0800

    資料庫改為連線雲端資料庫Atlas。

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
   ],
   runtimeConfig: {
     mongoDBURI: process.env.MONGODB_URI,
+    mongoDBCertificate: process.env.MONGODB_CERTIFICATE,
     jwtAccessSecret: process.env.JWT_ACCESS_TOKEN_SECRET,
     jwtRefreshSecret: process.env.JWT_REFRESH_TOKEN_SECRET,
   }
--- server/plugins/dbconnection.js
+++ server/plugins/dbconnection.js
@@ -3,11 +3,19 @@ import mongoose from "mongoose";
 export default async () => {
     const config = useRuntimeConfig()
     const connUri = config.mongoDBURI;
+    const certificate = config.mongoDBCertificate;
+    // console.log(__dirname);
     try {
         console.log(`嘗試連線資料庫:${connUri}...`);
-        // await mongoose.connect(config.dburl, DB_OPTIONS)
+
         mongoose.promise = global.Promise;
-        await mongoose.connect(connUri);
+        await mongoose.connect(connUri, {
+            tls: true,
+            tlsCertificateKeyFile: `./${certificate}`,
+            authMechanism: 'MONGODB-X509',
+            authSource: '$external',
+        });
+
         console.log('連線成功');
     } catch (error) {
         console.error('連線失敗，理由：', error);
